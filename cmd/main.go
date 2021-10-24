e3bcaa5abce6b4bf34e4b5c39038a9eacca61225
belousov.dmitriy <belousov.dmitriy@rt.ru>
2021-10-24 13:25:01 +0300

    server

---
+++ cmd/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/DmitriyBelousov/grpc/pkg/adder"
+	"github.com/DmitriyBelousov/grpc/pkg/api"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+func main(){
+	s  := grpc.NewServer()
+	adderSvc := &adder.GRPCServer{}
+	api.RegisterAdderServer(s, adderSvc)
+
+	l, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = s.Serve(l); err != nil{
+		log.Fatal(err)
+	}
+}
+
